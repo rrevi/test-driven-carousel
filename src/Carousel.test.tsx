f092e3661297aa77b59d9d83d2314561118b5b25
rrevi <primers.flowing-0q@icloud.com>
2025-05-26 06:52:01 -0400

    :sparkles: more implementation of Carousel

--- src/Carousel.test.tsx
+++ src/Carousel.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Carousel from "./Carousel";
 
 describe("Carousel", () => {
@@ -29,5 +30,33 @@ describe("Carousel", () => {
     render(<Carousel slides={slides} />);
     const img = screen.getByRole("img");
     expect(img).toHaveAttribute("src", slides[0].imgUrl);
-  })
+  });
+
+  it("reverses the slide when the Prev button is clicked", async () => {
+    render(<Carousel slides={slides} />);
+    const img = screen.getByRole("img");
+    const nextButton = screen.getByTestId("prev-button");
+    const user = userEvent.setup();
+
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[2].imgUrl);
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[1].imgUrl);
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[0].imgUrl);
+  });
+
+  it("advances the slide when the Next button is clicked", async () => {
+    render(<Carousel slides={slides} />);
+    const img = screen.getByRole("img");
+    const nextButton = screen.getByTestId("next-button");
+    const user = userEvent.setup();
+
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[1].imgUrl);
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[2].imgUrl);
+    await user.click(nextButton);
+    expect(img).toHaveAttribute("src", slides[0].imgUrl);
+  });
 })
--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import CarouselSlide from "./CarouselSlide";
+import CarouselButton from "./CarouselButton";
 
 type Slide = {
   imgUrl?: string;
@@ -8,9 +9,29 @@ type Slide = {
 }
 
 const  Carousel = ({ slides }: { slides?: Slide[] }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
   return (
     <div data-testid="carousel">
-      <CarouselSlide {...slides?.[0]} />
+      <CarouselSlide {...slides?.[slideIndex]} />
+      {}
+      <CarouselButton
+        data-testid="prev-button"
+        onClick={() => {
+          if(!slides) return;
+          setSlideIndex((i) => (i + slides.length - 1) % slides.length)
+        }}
+      >
+        Prev
+      </CarouselButton>
+      <CarouselButton
+        data-testid="next-button"
+        onClick={() => {
+          if (!slides) return;
+          setSlideIndex((i) => (i + 1) % slides.length);
+        }}
+      >
+        Next
+      </CarouselButton>
     </div>
   );
 }
